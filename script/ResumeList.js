a4e3c9773c504b91363eb8d3a028a6ced2c2dffd
marcorapg <marcorapg@gmail.com>
2014-11-19 22:42:13 -0200

    Lista de curriculos

---
+++ script/ResumeList.js
@@ -0,0 +1,32 @@
+$.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
+	$.each(data, function( index, value ) {
+		$('.slideshow').append(
+			$([
+				"<div style='display:" + (index == 0 ? "block" : "none") + ";' id='slide" + index + "' class='box'>",
+					"<p class='quote'>" + data[index].nome + "</p>",
+					"<p class='credit'>" + data[index].mini_curriculo +"</p>",
+				"</div>"
+			].join("\n"))
+		);
+	});
+});
+
+$(function () {
+  $('.slideshow div').hide(); // hide all slides
+  $('.slideshow div:first-child').show(); // show first slide
+		  
+	$( "#forward" ).click(function() {
+		$('.slideshow div:first-child').fadeOut(2000)
+									 .next('div')
+									 .fadeIn(3000)
+									 .end()
+									 .appendTo('.slideshow');
+	});
+
+	$( "#backward" ).click(function() {
+		$('.slideshow div:first-child').fadeOut(1000);
+									 
+		$('.slideshow div:last-child').fadeIn(3000).prependTo('.slideshow');
+
+	});	
+}); 
