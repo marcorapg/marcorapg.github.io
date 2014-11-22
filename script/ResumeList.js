631811667972e2851fa4a6993be1ad945e7956c6
marcorapg <marcorapg@gmail.com>
2014-11-22 14:54:55 -0200

    a

--- script/ResumeList.js
+++ script/ResumeList.js
@@ -1,33 +1,35 @@
 $.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
-	$('.slideshow').html('');
+	$('#slideshow').html('');
 	$.each(data, function( index, value ) {
-		$('.slideshow').append(
-			$([
-				"<div style='display:" + (index == 0 ? "block" : "none") + ";' id='slide" + index + "' class='box'>",
+		$('#slideshow').append(
+			$([			
+				"<div style='display:" + (index == 0 ? "block" : "none") + ";' id='slide" + index + "' class='box boxCurriculos'>",
 					"<p class='quote'>" + data[index].nome + "</p>",
 					"<p class='credit'>" + data[index].mini_curriculo +"</p>",
-				"</div>"
+				"</div>"				
 			].join("\n"))
 		);
 	});
 });
 
 $(function () {
-  $('.slideshow div').hide(); // hide all slides
-  $('.slideshow div:first-child').show(); // show first slide
+	$('#slideshow div:first-child').show(); // show first slide
 		  
 	$( "#forward" ).click(function() {
-		$('.slideshow div:first-child').fadeOut(2000)
-									 .next('div')
-									 .fadeIn(3000)
-									 .end()
-									 .appendTo('.slideshow');
+		$('#slideshow div:first-child').fadeOut(500).appendTo('#slideshow');
+		
+		setTimeout(function()
+		{
+			$('#slideshow div:first-child').fadeIn(500);
+		}, 500);
 	});
 
 	$( "#backward" ).click(function() {
-		$('.slideshow div:first-child').fadeOut(1000);
-									 
-		$('.slideshow div:last-child').fadeIn(3000).prependTo('.slideshow');
-
+		$('#slideshow div:first-child').fadeOut(500);
+		
+		setTimeout(function()
+		{
+			$('#slideshow div:last-child').fadeIn(500).prependTo('#slideshow');
+		}, 500);
 	});	
 });
