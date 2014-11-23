8272922c774835c0d5766ecf8fc9d222d8c07091
marcorapg <marcorapg@gmail.com>
2014-11-23 21:29:34 -0200

    a

--- script/ResumeList.js
+++ script/ResumeList.js
@@ -1,20 +1,24 @@
-$.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
-	$('#slideshow').html('');
-	$.each(data, function( index, value ) {
-		$('#slideshow').append(
-			$([			
-				"<div style='display:" + (index == 0 ? "block" : "none") + ";' id='slide" + index + "' class=''>",
-					"<a href=" + data[index].link_html + ">",
-						"<p class='quote'>" + data[index].nome + "</p>",
-					"</a>",
-					"<p class='credit'>" + data[index].mini_curriculo +"</p>",
-				"</div>"				
-			].join("\n"))
-		);
+$(document).ready(function(){
+	$.get( "http://rarolabs.com.br:88/alunos.json", function( data ) {
+		$('#slideshow').html('');
+		$.each(data, function( index, value ) {
+			$('#slideshow').append(
+				$([			
+					"<div style='display:" + (index == 0 ? "block" : "none") + ";' id='slide" + index + "' >",
+						"<a href='" + data[index].link_html + "'>",
+							"<p class='name'>" + data[index].nome + "</p>",
+						"</a>",
+						"<p class='resume'>" + data[index].mini_curriculo +"</p>",
+					"</div>"				
+				].join("\n"))
+			);
+		});
+		$('#leftName').text(data[data.length-1].nome);
+		$('#rightName').text(data[1].nome)
 	});
 });
 
-$(function () {
+$(document).ready(function(){
 	$('#slideshow div:first-child').show(); // show first slide
 		  
 	$( "#forward" ).click(function() {
@@ -23,6 +27,7 @@ $(function () {
 		setTimeout(function()
 		{
 			$('#slideshow div:first-child').fadeIn(500);
+			trocaNomes();
 		}, 500);
 	});
 
@@ -32,6 +37,13 @@ $(function () {
 		setTimeout(function()
 		{
 			$('#slideshow div:last-child').fadeIn(500).prependTo('#slideshow');
+			trocaNomes();
 		}, 500);
-	});	
+	});
+	
+	function trocaNomes() 
+	{
+		$('#leftName').text($('#slideshow div:last-child').find('p[class="name"]').text());
+		$('#rightName').text($('#slideshow div:first-child').next().find('p[class="name"]').text());
+	}
 });
